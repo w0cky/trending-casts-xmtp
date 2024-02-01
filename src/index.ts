6f71ab758eae56fb1438f56ca6bb1e40c5f0e7cf
jaerq <jaer.q@protonmail.com>
2024-02-01 15:40:27 -0600

    Run on start. Feedback messages added. Cleanup code

--- src/index.ts
+++ src/index.ts
@@ -37,16 +37,20 @@ function removeUrls(input: string): string {
 // Format a trending cast notification
 function formatTrendingNotification(cast: Cast): string {
     return `👤 ${cast.author.display_name} (@${cast.author.username})\n` +
-           `"${removeUrls(cast.text)}"\n` +
-           `https://warpcast.com/${cast.author.username}/${cast.hash.substring(0, 10)} \n\n`;
+        `"${removeUrls(cast.text)}"\n` +
+        `https://warpcast.com/${cast.author.username}/${cast.hash.substring(0, 10)} \n\n`;
+}
+
+async function sendStopMessage(context: BotContext): Promise<void> {
+    try {
+        await context.reply('You will stop receiving messages. Reply with !start to start again.')
+    } catch (error: any) {
+        console.error('Error fetching or sending trending casts:', error.message);
+    }
 }
 
 const app = express();
 const port = process.env.PORT || 4000;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
 
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
@@ -55,20 +59,25 @@ const server = app.listen(port, () => {
 // Initialize the bot
 run(async (context) => {
     const scheduledTime = process.env.CRON_TIME;
-    console.log(process.env);
     if (!scheduledTime) {
         throw new Error("CRON_TIME is undefined.");
     }
 
-    let job = new CronJob(
-        scheduledTime, // cronTime
-        () => sendTrendingCasts(context), // onTick
-        null, // onComplete
-        true, // start
-    );
-    switch (context.message.content) {
+    let job = CronJob.from(
+        {
+            cronTime: scheduledTime, // cronTime
+            onTick: () => sendTrendingCasts(context), // onTick
+            onComplete: () => sendStopMessage(context), // onComplete
+            start: true, // start
+            runOnInit: true
+        });
+    switch (context.message.content.trim()) {
         case '!start':
+            context.reply('Started. You will receive the top 3 trending casts daily. Type !stop to stop.')
             job.start();
+            break;
+        case '!next':
+            context.reply(`${job.nextDate().diffNow('minutes')} min. until the next update`)
             break;
         case '!stop':
             job.stop();
